04b82f29300979923fcdc31b4649339ea22f1dd1
jdecker77 <jdecker77@gmail.com>
2023-08-25 09:37:51 -0400

    Initial Commit

---
+++ MatchGame/MatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGameApp.swift
+//  MatchGame
+//
+//  Created by Jesse Decker on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
