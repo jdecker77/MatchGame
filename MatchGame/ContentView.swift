3d949c7c227f3a17090eba928d055a6794fcfc18
jdecker77 <jdecker77@gmail.com>
2023-08-26 10:35:43 -0400

    Finished Part 2

--- MatchGame/ContentView.swift
+++ MatchGame/ContentView.swift
@@ -8,59 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🐶","🐣","🐷","🦄","🙈","🐛","🐹","🐸","🐔","🦕","🦈","🐙"]
-    
-    @State var cardCount: Int = 4
-    
+
+    @State var emojis: Array<String> = []
+        
     var body: some View {
         VStack {
-            cards
+            Text("The Match Game!")
+                .font(.largeTitle)
+            ScrollView {
+                cards
+            }
             Spacer()
-            cardCountAdjusters
+            themeSelection
+        }
+        .padding()
+    }
+    
+    func setTheme(by theme: String, symbol: String, by emoji: [String]) -> some View {
+        VStack {
+            
+            Button(action: {
+                var paired_emoji: Array<String>
+                paired_emoji = emoji+emoji
+                emojis = paired_emoji.shuffled()
+            }, label: {
+                Image(systemName: symbol)
+                    .frame(height: 20.0,alignment: .bottom)
+                    .imageScale(.large)
+                    
+            })
+            Text(theme)
+                .font(.caption)
+                .foregroundColor(Color.blue)
+        }
+    }
+    
+    var theme1: some View {
+        setTheme(by: "House Pets",symbol:"house",by: ["🐶","🐣","🐕‍🦺","🐠","🐹","🐛","🐩","🐸"])
+    }
+    
+    var theme2: some View {
+        setTheme(by: "Workers",symbol:"wrench.fill",by: ["🦄","🦜","🐴","🐔","🐐","🐂","🦆","🦚","🐖","🐏"])
+    }
+    
+    var theme3: some View {
+        setTheme(by: "Swimmers",symbol:"figure.open.water.swim",by: ["🦕","🦈","🐙","🦀","🐬","🐳","🐊","🦭","🦐","🦞","🐢","🪼"])
+    }
+    
+    var themeSelection: some View {
+        HStack {
+            Spacer()
+            theme1
+            Spacer()
+            theme2
+            Spacer()
+            theme3
+            Spacer()
+        
         }
         .padding()
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-            ForEach(0..<cardCount, id: \.self){ index in
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
+            ForEach(0..<emojis.count, id: \.self){ index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol:"rectangle.stack.fill.badge.minus")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol:"rectangle.stack.fill.badge.plus")
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
